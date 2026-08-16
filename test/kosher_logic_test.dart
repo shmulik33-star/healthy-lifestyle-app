003b67ec642c0e646a2ccab786fd2bfcde5acfe8
shmulik33-star <shmulik33@gmail.com>
2026-08-16 22:00:45 +0300

    test: cover configurable meat-to-dairy waiting time

--- test/kosher_logic_test.dart
+++ test/kosher_logic_test.dart
@@ -2,4 +2,34 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:healthy_lifestyle_stage9/shared/data/food_catalog.dart';
 import 'package:healthy_lifestyle_stage9/shared/models/app_state.dart';
 
-void main(){test('adding meat creates six-hour dairy wait',(){final s=AppState();final chicken=foodCatalog.firstWhere((f)=>f.id=='chicken');s.addFood(chicken,1,'חתיכה בינונית');expect(s.lastMeatTime,isNotNull);expect(s.dairyAllowedAt!.difference(s.lastMeatTime!).inMinutes,360);});}
+void main() {
+  test('dairy wait follows the user profile setting', () {
+    final state = AppState()
+      ..kosherEnabled = true
+      ..meatDairySeparationEnabled = true
+      ..meatWaitMinutes = 180;
+
+    final chicken = foodCatalog.firstWhere((food) => food.id == 'chicken');
+    state.addFood(chicken, 1, 'חתיכה בינונית');
+
+    expect(state.lastMeatTime, isNotNull);
+    expect(
+      state.dairyAllowedAt!.difference(state.lastMeatTime!).inMinutes,
+      180,
+    );
+    expect(state.dairyAllowed, isFalse);
+  });
+
+  test('dairy wait can be disabled by the user', () {
+    final state = AppState()
+      ..kosherEnabled = true
+      ..meatDairySeparationEnabled = false
+      ..meatWaitMinutes = 360;
+
+    final chicken = foodCatalog.firstWhere((food) => food.id == 'chicken');
+    state.addFood(chicken, 1, 'חתיכה בינונית');
+
+    expect(state.dairyAllowed, isTrue);
+    expect(state.dairyAllowedAt, isNull);
+  });
+}
